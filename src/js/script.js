5998c424ff3857ed3410342fafd89797d9dadc2c
johannahyll <johanna.hyll@gmail.com>
2025-02-03 20:08:06 +0100

    b

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,4 @@
-"use strict"
-
 "use strict";
-
-// Hämta in meny-knapparna
-let openBtn = document.getElementById("open-menu");
-let closeBtn = document.getElementById("close-menu");
-
-// Eventlyssnare
-openBtn.addEventListener('click', toggleMenu);
-closeBtn.addEventListener('click', toggleMenu);
-
-// Toggla fram navigeringsmenyn
-function toggleMenu() {
-    let navMenuEl = document.getElementById("nav-menu");
-
-    // Hämta in css för menyn
-    let style = window.getComputedStyle(navMenuEl);
-
-    // Kolla om navigeringen är synlig eller ej, och ändra display
-    if (style.display === "none") {
-        navMenuEl.style.display = "flex";
-    } else {
-        navMenuEl.style.display = "none";
-    }
-}
-
-
 
 // Open the Modal
 function openModal() {
